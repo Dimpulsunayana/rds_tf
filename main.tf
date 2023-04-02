ac3bc113914c451b696fb57d2a5a471251da44a8
windows 10 <dimpulsunayana205@gmail.com>
2023-04-02 13:16:41 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -1,13 +1,63 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = var.engine_version
-  instance_class       = var.instance_class
-  username             = "dimpul"
-  password             = "dimpul123"
-  parameter_group_name = var.parameter_group_name
-  skip_final_snapshot  = true
+resource "aws_security_group" "rds" {
+  name        = "${var.env}-rds_segrp"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.main_vpc
+
+  ingress {
+    description      = "mysql"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags       = merge(
+    local.common_tags,
+    { Name = "${var.env}-rds_segrp" }
+  )
+}
+
+resource "aws_docdb_subnet_group" "rds" {
+  name       = "${var.env}-rds_subnetgrp"
+  subnet_ids = var.subnet_ids
+
+  tags       = merge(
+    local.common_tags,
+    { name = "${var.env}-rds_subnetgrp" }
+  )
+}
+
+resource "aws_rds_cluster" "rds" {
+  cluster_identifier      = "${var.env}-rds"
+  engine                  = var.engine
+  engine_version          = var.engine_version
+  database_name           = "mydsql"
+  master_username         = "dimpul"
+  master_password         = "dimpul1"
+  db_subnet_group_name   = aws_docdb_subnet_group.rds.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  storage_encrypted = true
+  tags       = merge(
+    local.common_tags,
+    { name = "${var.env}-rds_cluster" }
+  )
+}
+
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "aurora-cluster-rds-${count.index}"
+  cluster_identifier = aws_rds_cluster.rds.id
+  instance_class     = var.instance_class
+  engine             = aws_rds_cluster.rds.engine
+  engine_version     = aws_rds_cluster.rds.engine_version
+
   tags       = merge(
     local.common_tags,
     { name = "${var.env}-rds_cluster" }
--- var.tf
+++ var.tf
@@ -2,3 +2,7 @@ variable "env" {}
 variable "instance_class" {}
 variable "engine_version" {}
 variable "parameter_group_name" {}
+variable "subnet_ids" {}
+variable "allow_cidr" {}
+variable "main_vpc" {}
+variable "engine" {}
