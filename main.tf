ac471d1d01a602fd3c94360482f72d00125569b3
windows 10 <dimpulsunayana205@gmail.com>
2023-04-03 14:47:42 +0530

    updated user repo

--- main.tf
+++ main.tf
@@ -41,6 +41,9 @@ resource "aws_rds_cluster" "rds" {
   database_name           = "mysql"
   master_username         = "dimpul"
   master_password         = "dimpul123"
+  //storage_type              = "io1"
+  allocated_storage         = 20
+  iops                      = 1000
   db_subnet_group_name   = aws_docdb_subnet_group.rds.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   storage_encrypted = true
